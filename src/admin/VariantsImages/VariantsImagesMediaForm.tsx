217c5688e87d265617c13ca0aca7f403353cc6cc
Anouar Kacem <kcm.anouar@gmail.com>
2024-04-21 14:12:37 +0100

    add actionnables

--- src/admin/VariantsImages/VariantsImagesMediaForm.tsx
+++ src/admin/VariantsImages/VariantsImagesMediaForm.tsx
@@ -1,10 +1,11 @@
-import clsx from 'clsx';
-import { Controller, FieldArrayWithId, useFieldArray } from 'react-hook-form';
-import { NestedForm } from './utils/nestedForm';
-import { FormImage } from './utils/images';
-import FileUploadField from './components/FileUploadField';
-import { CheckCircleSolid } from '@medusajs/icons';
-import { useRef } from 'react';
+import clsx from "clsx";
+import { Controller, FieldArrayWithId, useFieldArray } from "react-hook-form";
+import { NestedForm } from "./utils/nestedForm";
+import { FormImage } from "./utils/images";
+import FileUploadField from "./components/FileUploadField";
+import { CheckCircleSolid, Trash } from "@medusajs/icons";
+import { useRef } from "react";
+import Actionables, { ActionType } from "./components/common/actionables";
 
 type ImageType = { selected: boolean } & FormImage;
 
@@ -14,16 +15,16 @@ export type MediaFormType = {
 
 type Props = {
   form: NestedForm<MediaFormType>;
-  type: 'thumbnail' | 'media';
+  type: "thumbnail" | "media";
 };
 
 const VariantsImagesMediaForm = ({ form, type }: Props) => {
   const { control, path, setValue } = form;
 
-  const singleSelection = type === 'thumbnail';
-  const { fields, append } = useFieldArray({
+  const singleSelection = type === "thumbnail";
+  const { fields, remove, replace, append } = useFieldArray({
     control: control,
-    name: path('images'),
+    name: path("images"),
   });
 
   const prevSelectedImage = useRef<number | undefined>(
@@ -59,7 +60,7 @@ const VariantsImagesMediaForm = ({ form, type }: Props) => {
             onFileChosen={handleFilesChosen}
             placeholder="1200 x 1600 (3:4) recommended, up to 10MB each"
             multiple
-            filetypes={['image/gif', 'image/jpeg', 'image/png', 'image/webp']}
+            filetypes={["image/gif", "image/jpeg", "image/png", "image/webp"]}
             className="py-large"
           />
         </div>
@@ -69,7 +70,7 @@ const VariantsImagesMediaForm = ({ form, type }: Props) => {
           <div className="mb-small">
             <h2 className="inter-large-semibold mb-2xsmall">Uploads</h2>
             <p className="inter-base-regular text-grey-50 mb-large">
-              {type === 'thumbnail' ? (
+              {type === "thumbnail" ? (
                 <span>Select an image to use as variant thumbnail.</span>
               ) : (
                 <span>Select images to use as variant images.</span>
@@ -85,6 +86,7 @@ const VariantsImagesMediaForm = ({ form, type }: Props) => {
                   index={index}
                   form={form}
                   onSelected={handleImageSelected}
+                  remove={remove}
                 />
               );
             })}
@@ -96,60 +98,77 @@ const VariantsImagesMediaForm = ({ form, type }: Props) => {
 };
 
 type ImageProps = {
-  image: FieldArrayWithId<MediaFormType, 'images', 'id'>;
+  image: FieldArrayWithId<MediaFormType, "images", "id">;
   index: number;
   form: NestedForm<MediaFormType>;
   onSelected: (index: number) => void;
+  remove: (index: number) => void;
 };
 
-const Image = ({ image, index, form, onSelected }: ImageProps) => {
+const Image = ({ image, index, form, onSelected, remove }: ImageProps) => {
   const { control, path } = form;
-
+  const actions: ActionType[] = [
+    {
+      label: "Delete",
+      onClick: () => remove(index),
+      icon: <Trash />,
+      variant: "danger",
+    },
+  ];
   return (
-    <Controller
-      name={path(`images.${index}.selected`)}
-      control={control}
-      render={({ field: { value, onChange } }) => {
-        return (
-          <div className="relative">
-            <button
-              className={clsx(
-                'hover:bg-grey-5 rounded-rounded group flex items-center justify-between',
-                {
-                  'bg-grey-5': value,
-                }
-              )}
-              type="button"
-              onClick={() => {
-                onChange(!value);
-                if (!value) {
-                  onSelected(index);
-                }
-              }}
-            >
-              <div className="gap-x-large flex items-center">
-                <div className="flex h-32 w-32 items-center justify-center">
-                  <img
-                    src={image.url}
-                    alt={image.name || 'Uploaded image'}
-                    className="rounded-rounded max-w-32 max-h-32"
-                  />
-                </div>
+    <>
+      <Controller
+        name={path(`images.${index}.selected`)}
+        control={control}
+        render={({ field: { value, onChange } }) => {
+          return (
+            <>
+              <div>
+                <div className="relative">
+                  <button
+                    className={clsx(
+                      "hover:bg-grey-5 rounded-rounded group flex items-center justify-between",
+                      {
+                        "bg-grey-5": value,
+                      }
+                    )}
+                    type="button"
+                    onClick={() => {
+                      onChange(!value);
+                      if (!value) {
+                        onSelected(index);
+                      }
+                    }}
+                  >
+                    <div className="gap-x-large flex items-center">
+                      <div className="flex h-32 w-32 items-center justify-center">
+                        <img
+                          src={image.url}
+                          alt={image.name || "Uploaded image"}
+                          className="rounded-rounded max-w-32 max-h-32"
+                        />
+                      </div>
 
-                <span
-                  className={clsx('hidden', {
-                    '!text-violet-60 bottom-xsmall right-xsmall absolute !block':
-                      value,
-                  })}
-                >
-                  <CheckCircleSolid />
-                </span>
+                      <span
+                        className={clsx("hidden", {
+                          "!text-violet-60 bottom-xsmall right-xsmall absolute !block":
+                            value,
+                        })}
+                      >
+                        <CheckCircleSolid />
+                      </span>
+                    </div>
+                  </button>
+                </div>
+                <div className="mt-4">
+                  <Actionables actions={actions} forceDropdown />
+                </div>
               </div>
-            </button>
-          </div>
-        );
-      }}
-    />
+            </>
+          );
+        }}
+      />
+    </>
   );
 };
 
